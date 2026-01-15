1b8f3ea4242e555934f10ee70949a7440033ff37
wlewis0991 <wlewis1990@gmail.com>
2026-01-14 19:32:26 -0800

    Modules for other pages added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 // src/index.js
 import "./styles.css";
-import { greeting } from "./greeting.js";
+import { greeting } from "./home.js"
+
+const homeButton = document.querySelector(".home");
+const menuButton = document.querySelector(".menu");
+const aboutButton = document.querySelector(".about")
+const contactButton = document.querySelector(".contact")
+const content = document.querySelector(".content");
+
+homeButton.addEventListener("click" , () => {
+    content.textContent="";
+});
+
 
 console.log(greeting);
